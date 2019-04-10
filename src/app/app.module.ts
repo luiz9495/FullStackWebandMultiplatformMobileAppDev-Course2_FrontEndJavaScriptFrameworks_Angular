7fae9472efe0b53fe45b7e8b75732cad671d8945
Luiz Cunha <luiz9495@gmail.com>
2019-04-10 20:42:06 +0900

    Assignment 2: Angular Services, Routing and Single Page Applications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -47,7 +48,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
